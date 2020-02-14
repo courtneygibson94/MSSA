edc2d2ab314a4e43d35c3de94a85eb86a1a40588
courtneygibson94 <54868522+courtneygibson94@users.noreply.github.com>
2020-02-14 11:43:01 -0600

    Add files via upload

---
+++ petData/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace petData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textFN = new System.Windows.Forms.TextBox();
+            this.textLN = new System.Windows.Forms.TextBox();
+            this.textEM = new System.Windows.Forms.TextBox();
+            this.textPN = new System.Windows.Forms.TextBox();
+            this.textNO = new System.Windows.Forms.TextBox();
+            this.textOWN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textFN
+            // 
+            this.textFN.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFN.Location = new System.Drawing.Point(290, 144);
+            this.textFN.Name = "textFN";
+            this.textFN.Size = new System.Drawing.Size(311, 62);
+            this.textFN.TabIndex = 1;
+            // 
+            // textLN
+            // 
+            this.textLN.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLN.Location = new System.Drawing.Point(290, 253);
+            this.textLN.Name = "textLN";
+            this.textLN.Size = new System.Drawing.Size(311, 62);
+            this.textLN.TabIndex = 2;
+            // 
+            // textEM
+            // 
+            this.textEM.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEM.Location = new System.Drawing.Point(290, 373);
+            this.textEM.Name = "textEM";
+            this.textEM.Size = new System.Drawing.Size(311, 62);
+            this.textEM.TabIndex = 2;
+            // 
+            // textPN
+            // 
+            this.textPN.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPN.Location = new System.Drawing.Point(290, 505);
+            this.textPN.Name = "textPN";
+            this.textPN.Size = new System.Drawing.Size(311, 62);
+            this.textPN.TabIndex = 2;
+            // 
+            // textNO
+            // 
+            this.textNO.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNO.Location = new System.Drawing.Point(290, 625);
+            this.textNO.Name = "textNO";
+            this.textNO.Size = new System.Drawing.Size(311, 62);
+            this.textNO.TabIndex = 2;
+            // 
+            // textOWN
+            // 
+            this.textOWN.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOWN.Location = new System.Drawing.Point(290, 776);
+            this.textOWN.Name = "textOWN";
+            this.textOWN.Size = new System.Drawing.Size(311, 62);
+            this.textOWN.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 55);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 55);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 505);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 55);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Mobile";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 632);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 55);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Notes";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 776);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(214, 55);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "OwnerID";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Submit.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Submit.Location = new System.Drawing.Point(987, 350);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(210, 71);
+            this.Submit.TabIndex = 3;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Menu;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(307, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(624, 66);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Owner Registration";
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1380, 871);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.textOWN);
+            this.Controls.Add(this.textNO);
+            this.Controls.Add(this.textPN);
+            this.Controls.Add(this.textEM);
+            this.Controls.Add(this.textLN);
+            this.Controls.Add(this.textFN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Submit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textFN;
+        private System.Windows.Forms.TextBox textLN;
+        private System.Windows.Forms.TextBox textEM;
+        private System.Windows.Forms.TextBox textPN;
+        private System.Windows.Forms.TextBox textNO;
+        private System.Windows.Forms.TextBox textOWN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ petData/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace petData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public string conString = "Data Source=MS-002126362353\\SQLEXPRESS;Initial Catalog=PetCareSolutionsDB;Integrated Security=True";
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(conString))
+            {
+                sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("Registration", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure; 
+                sqlCmd.Parameters.AddWithValue("@LastName", textLN.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@FirstName", textFN.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Email", textEM.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Mobile", textPN.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Notes", textNO.Text.Trim());
+                
+                sqlCmd.Parameters.AddWithValue("@OwnerID", textOWN.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Entry Successsful");
+                Clear();
+            }
+               
+            
+        }
+        void Clear()
+        {
+            textLN.Text = textFN.Text = textEM.Text = textPN.Text = textNO.Text = "";
+        }
+
+    }
+}
